a9e3b0f8ee5dad33351feecddbb570ec4c65d4ec
albukhari21 <albukhariinsankamil@gmail.com>
2025-05-16 09:03:45 +0700

    menerapkan traverse untuk menampilkan nomor daftar siswa

--- doubleLinkedList.cpp
+++ doubleLinkedList.cpp
@@ -147,9 +147,13 @@ public:
 
             cout << i + 1 << "." << currentNode->noMhs << " " << endl;
 
-            
+            // Step 3: Move to next node
+            currentNode = currentNode->next;
+            i++;
         }
     }
+
+    void traverse
 
 
 
