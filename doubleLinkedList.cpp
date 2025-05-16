3f3210a772cd7b414f8a650babb6bbdb31759faf
albukhari21 <albukhariinsankamil@gmail.com>
2025-05-16 08:11:09 +0700

    menambahkan class node pada variable noMhs

--- doubleLinkedList.cpp
+++ doubleLinkedList.cpp
@@ -2,3 +2,20 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class Node 
+{
+public:
+    int noMhs;
+    Node *next;
+    Node *prev;
+};
+
+class DoubleLinkedList
+{
+private:
+    Node *START;
+
+public;
+    DoubleLinkedList
+}
